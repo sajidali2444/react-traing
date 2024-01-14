7b04e708abaf850d32506d50131781e708de5a03
makif8504 <akifmalhi786@gmail.com>
2024-01-14 23:45:10 +0500

    learn map method of array

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/Button';
+import TodoAppForm from './components/TodoForm';
+
+function App() {
+  return (
+    <>
+      <TodoAppForm />
+      <Button />
+    </>
+  );
+}
+
+export default App;
