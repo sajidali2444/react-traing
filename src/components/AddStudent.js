0e13a87250f58eb0e0b3ec9bd51b196efd4828be
Sajid <sajidali2444@gmail.com>
2024-02-06 00:21:22 +0500

    delete student in local storage

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,3 +1,125 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+const storageKey = 'students';
 export default function AddStudent() {
-  return <h1>Add student page</h1>;
+  const navigate = useNavigate();
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fatherName, setFatherName] = useState('');
+  const [mobile, setMobile] = useState('');
+  const getFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
+  const getLastName = (event) => {
+    setLastName(event.target.value);
+  };
+  const getFatherName = (event) => {
+    setFatherName(event.target.value);
+  };
+  const getMobile = (event) => {
+    setMobile(event.target.value);
+  };
+
+  const onSubmit = () => {
+    //const student = { firstName, lastName, fatherName, mobile };
+    const student = {
+      firstName: firstName,
+      lastName: lastName,
+      fatherName: fatherName,
+      mobile: mobile,
+    };
+
+    const oldStudent = localStorage.getItem(storageKey);
+    if (oldStudent !== null) {
+      const oldData = JSON.parse(oldStudent);
+      student.rollNumber = oldData.length + 1;
+      student.id = oldData.length + 1;
+      oldData.push(student);
+      const newData = JSON.stringify(oldData);
+      localStorage.setItem(storageKey, newData);
+    } else {
+      student.rollNumber = 1;
+      student.id = 1;
+      const studentData = JSON.stringify([student]);
+      localStorage.setItem(storageKey, studentData);
+    }
+
+    navigate('/');
+  };
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <h1>Add student page</h1>
+      <form>
+        <div style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ display: 'flex', gap: '10px', marginBottom: '15px' }}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '200px',
+              }}
+            >
+              <label> First Name </label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                onChange={getFirstName}
+              ></input>
+              <label> Last Name </label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                onChange={getLastName}
+              ></input>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '200px',
+              }}
+            >
+              <label> Father Name </label>
+              <input
+                type="text"
+                id="fatherName"
+                name="fatherName"
+                onChange={getFatherName}
+              ></input>
+              <label> Mobile </label>
+              <input
+                type="text"
+                id="mobile"
+                name="mobile"
+                onChange={getMobile}
+              ></input>
+            </div>
+          </div>
+
+          <div style={{ marginTop: '10px', marginBottom: '20px' }}>
+            <button
+              onClick={onSubmit}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                marginTop: '10px',
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,66 @@
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  /*
+  const students = Array.from({ length: 10 }).map((item, index) => ({
+    id: index,
+    firstName: `Student ${index}`,
+    lastName: `Student ${index}`,
+    rollNumber: index + index,
+  }));
+  */
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    debugger;
+    const studentList = localStorage.getItem('students');
+    if (studentList !== null) {
+      const studentData = JSON.parse(studentList);
+      //setStudents((prev) => [...prev, ...studentData]);
+      setStudents(studentData);
+    }
+  }, []);
+
+  const removeStudent = (id) => {
+    const remainingStudents = [];
+    for (let i = 0; i < students.length; i++) {
+      const student = students[i];
+      if (student.id === id) {
+        continue;
+      } else {
+        remainingStudents.push(student);
+      }
+    }
+    //students.push(remainingStudents);
+    setStudents(remainingStudents);
+  };
+
+  return (
+    <>
+      <h1>Students</h1>
+      <table border={'1'} style={{ width: '800px' }}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Full name</th>
+            <th>Roll Number</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((item, index) => (
+            <tr>
+              <td>{item.id}</td>
+              <td>{`${item.firstName} ${item.lastName}`}</td>
+              <td>{item.rollNumber}</td>
+              <td>
+                <button>Edit</button>
+                <button onClick={() => removeStudent(item.id)}>X</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
